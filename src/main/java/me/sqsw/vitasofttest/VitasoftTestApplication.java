f051d024182683d98f9c7b7ef1cbe42eb893a3a1
Svetlana Antoshkina <svetlana120595@gmail.com>
2024-05-04 08:49:08 +0300

    Spring template

---
+++ src/main/java/me/sqsw/vitasofttest/VitasoftTestApplication.java
@@ -0,0 +1,13 @@
+package me.sqsw.vitasofttest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VitasoftTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VitasoftTestApplication.class, args);
+	}
+
+}
